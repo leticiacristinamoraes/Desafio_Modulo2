9221de34d85b8fb69914637b2b4344679e213c54
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2021-09-06 16:57:56 -0300

    Entrega máxima parcial: excluir um funcionário através do cpf.

--- src/br/com/zup/Main.java
+++ src/br/com/zup/Main.java
@@ -14,12 +14,12 @@ public class Main {
         int opcaoDesejada = 0;
         int chave = 0;
 
-
         while (chave == 0) {
 
             System.out.println("Digite 1 para cadastrar um novo funcionário.");
             System.out.println("Digite 2 para visualizar a lista de funcionários.");
-            System.out.println("Digite 3 para sair do programa.");
+            System.out.println("Digite 3 para remover um funcionário cadastrado utilizando o CPF.");
+            System.out.println("Digite 4 para sair do programa.");
             opcaoDesejada = leitor.nextInt();
             leitor.nextLine();
 
@@ -38,13 +38,32 @@ public class Main {
                     break;
                 case 2:
                     for (String verificarLista : listaDeFuncionarios.keySet()) {
-                        System.out.println("CPF: " + verificarLista + "\n"+ listaDeFuncionarios.get(verificarLista));
+                        System.out.println("CPF: " + verificarLista + "\n" + listaDeFuncionarios.get(verificarLista));
                     }
                     break;
                 case 3:
+                    System.out.println("Digite o CPF do fucnionário que deseja remover da lista:");
+                    String cpfDeletado = "";
+                    String deletarCpf = leitor.nextLine();
+
+                    for (String verificarCpf : listaDeFuncionarios.keySet()) {
+                        if (verificarCpf.equals(deletarCpf)) {
+                            System.out.println("A exclusão do cadastro foi realizada com sucesso!");
+                            cpfDeletado = deletarCpf;
+                            listaDeFuncionarios.remove(cpfDeletado);
+                        } else {
+                            System.out.println("O CPF digitado não pode ser encontrado. Por favor, digite um CPF válido.");
+                        }
+                    }
+                    break;
+
+                case 4:
                     chave = 1;
                     System.out.println("Obrigada por utilizar nosso sistema.");
                     break;
+                default:
+                System.out.println("Digite uma opção válida!");
+                break;
             }
 
         }
