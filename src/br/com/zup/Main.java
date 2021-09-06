88b254b18b0b4634bf999120340da6255eb14c8d
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2021-09-06 09:33:42 -0300

    Entrega mínima: cadastrando pelo menos 1 funcionário.

--- src/br/com/zup/Main.java
+++ src/br/com/zup/Main.java
@@ -1,8 +1,28 @@
 package br.com.zup;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Olá, seja bem vinde ao sistema Estrela.");
+
+        Map<String, String> listaDeFuncionarios = new HashMap<String, String>();
+
+        System.out.println("Para cadastrar um novo funcionário, digite o CPF do colaborador:");
+        String cpf = leitor.nextLine();
+        System.out.println("Digite agora o nome completo do colaborador:");
+        String nomeFuncionario = leitor.nextLine();
+        System.out.println("Digite o e-mail do colaborador:");
+        String email = leitor.nextLine();
+        System.out.println("Para finalizar o cadastro, digite o telefone do colaborador:");
+        String telefone = leitor.nextLine();
+
+        listaDeFuncionarios.put("CPF do colaborador:" + cpf, "\nNome do colaborador: " + nomeFuncionario + "\nEmail: " + email + "\nTelefone: " + telefone);
+        System.out.println(listaDeFuncionarios);
     }
 }
